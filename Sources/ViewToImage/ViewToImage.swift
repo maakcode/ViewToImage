71fc2e9711a020247a2e315345984c8a747f3b45
MaakCode <11623298+maakcode@users.noreply.github.com>
2023-07-16 15:34:03 +0900

    Add helper methods

--- Sources/ViewToImage/ViewToImage.swift
+++ Sources/ViewToImage/ViewToImage.swift
@@ -1,6 +1,34 @@
-public struct ViewToImage {
-    public private(set) var text = "Hello, World!"
+#if os(iOS)
+import UIKit
 
-    public init() {
+public extension UIView {
+    func asImage() -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(bounds.size, false, window?.screen.scale ?? 1)
+
+        if let context = UIGraphicsGetCurrentContext() {
+            layer.render(in: context)
+        }
+
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return image
     }
 }
+
+#elseif os(macOS)
+import Cocoa
+
+public extension NSView {
+    func asImage() -> NSImage? {
+        NSImage(size: bounds.size, flipped: false) { [weak self] rect in
+            guard let self,
+                  let rep = bitmapImageRepForCachingDisplay(in: rect)
+            else { return false }
+
+            cacheDisplay(in: rect, to: rep)
+            return rep.draw()
+        }
+    }
+}
+#endif
